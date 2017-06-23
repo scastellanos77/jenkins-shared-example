27ea95be71870aec9ffcae807199a889973867d4
leszko <rafal.leszko@gmail.com>
2017-06-23 20:53:02 +0200

    Fix

--- vars/commitPipeline.groovy
+++ vars/commitPipeline.groovy
@@ -4,15 +4,22 @@
 def call(Map templateParams) {   
 pipeline {
         agent any
+        parameters {
+            string(name: 'myInput', description: 'Some pipeline parameters')
+        }
         stages {
-            stage('Build') {
+            stage('Stage one') {
                 steps {
-			echo "ala"
+                    script {
+                        echo "Parameter from template creation: " + templateParams.someParam
+                    }
                 }
             }
-            stage('Unit test') {
+            stage('Stage two') {
                 steps {
-			echo "ola"
+                    script {
+                        echo "Job input parameter: " + params.myInput
+                    }
                 }
             }
         }
